430316d14d489a2e3c89943ae157cbb6ef1f2fcb
Ubuntu <ubuntu@ip-172-31-85-124.ec2.internal>
2020-03-23 21:17:16 +0900

    add mini project

---
+++ mini-project/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8001;
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.render('index', { })
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ mini-project/public/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(event) {   
+  const address = document.getElementById('address');
+  const search = document.getElementById('search');
+  const result = document.getElementById('result');
+
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var latitude = pos.coords.latitude;
+    var longitude = pos.coords.longitude;
+    alert("현재 위치는 : " + latitude + ", "+ longitude);
+  });
+  
+  address.onkeypress = (e) => {
+    if (e.keyCode == 13) {
+      search.onclick();
+      search.classList.add("hover");
+    }
+  }
+  address.onkeyup = (e) => {
+    search.classList.remove("hover");
+  }
+  search.onclick = () => {
+    const value = address.value;
+    const url = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address=";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        result.innerHTML = "";
+        data = JSON.parse(xhttp.responseText);
+        for (let {addr, name} of data.stores) {
+          const li = document.createElement('li');
+          li.innerHTML = `<h3>${name}</h3><p>${addr}</p>`;
+          result.append(li);
+        }
+      }
+    };
+    xhttp.open("GET", url+value, true);
+    xhttp.send();
+  }
+});
